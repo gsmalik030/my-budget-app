bb53cca9f458cf1dea6f3d401b2c679b7935714b
Ghulam Subbhani <gsmalik030@gmail.com>
2023-05-31 15:58:25 +0200

    Added styles

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
+  
   devise_for :users
-  root to: 'groups#index'
-  get 'groups/index', to: 'groups#index'
-
-  # get 'users/index', to: 'users#index'
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
@@ -12,4 +9,5 @@ Rails.application.routes.draw do
   resources :groups do
     resources :entities
   end
+  root "home#index"
 end
