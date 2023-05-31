99506e4d89503a0fa7c83518720ceeef9a748a95
Ghulam Subbhani <gsmalik030@gmail.com>
2023-05-31 16:02:51 +0200

    Fixed rubocop errors

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  
   devise_for :users
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
@@ -9,5 +8,5 @@ Rails.application.routes.draw do
   resources :groups do
     resources :entities
   end
-  root "home#index"
+  root 'home#index'
 end
