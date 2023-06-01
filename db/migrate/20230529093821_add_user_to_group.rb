a1cba43895e6193c49cc134caa34e0a749f58a1d
Ghulam Subbhani <gsmalik030@gmail.com>
2023-06-01 09:23:57 +0200

    Fixed the spelling mistake

--- db/migrate/20230529093821_add_user_to_group.rb
+++ db/migrate/20230529093821_add_user_to_group.rb
@@ -1,5 +1,6 @@
-class AddUserToEntity < ActiveRecord::Migration[7.0]
+class AddUserToGroup < ActiveRecord::Migration[7.0]
   def change
     add_reference :groups, :user, null: false, foreign_key: true
   end
 end
+
